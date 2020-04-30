97901e0fbfd02a3a13cf0e9bf7e00e1fec37bd8b
Naman Aggarwal <naman.aggarwal@yahoo.com>
2020-04-30 15:17:28 +0800

    payload should be stringified

--- todo.ts
+++ todo.ts
@@ -1,52 +1,56 @@
 class ToDo {
   static BASE_URL: string = 'https://outlook.office.com/api/v2.0/me'
 
+  // eslint-disable-next-line camelcase
   private readonly fetchApp: GoogleAppsScript.URL_Fetch.UrlFetchApp
+
   private readonly accessToken: string
 
+  // eslint-disable-next-line camelcase
   constructor(fetchApp: GoogleAppsScript.URL_Fetch.UrlFetchApp, accessToken: string) {
-    this.accessToken = accessToken
-    this.fetchApp = fetchApp
+    this.accessToken = accessToken;
+    this.fetchApp = fetchApp;
   }
 
   static getAuthHeaders(accessToken: string) {
     return {
-      'Authorization': `Bearer ${accessToken}`
+      Authorization: `Bearer ${accessToken}`,
     };
   }
 
   getAllFolders() {
+    // eslint-disable-next-line camelcase
     const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
-      headers: ToDo.getAuthHeaders(this.accessToken)
-    }
-    const response = this.fetchApp.fetch(`${ToDo.BASE_URL}/taskfolders`, options)
-    return JSON.parse(response.getContentText())
+      headers: ToDo.getAuthHeaders(this.accessToken),
+    };
+    const response = this.fetchApp.fetch(`${ToDo.BASE_URL}/taskfolders`, options);
+    return JSON.parse(response.getContentText());
   }
 
   createTask(folderID: string, taskSubject: string, date: string) {
     const data = {
-      "Subject": taskSubject,
-      "StartDateTime": {
-        "DateTime": date,
-        "TimeZone": "Pacific Standard Time"
+      Subject: taskSubject,
+      StartDateTime: {
+        DateTime: date,
+        TimeZone: 'Pacific Standard Time',
       },
-      "DueDateTime": {
-        "DateTime": date,
-        "TimeZone": "Pacific Standard Time"
-      }
-    }
+      DueDateTime: {
+        DateTime: date,
+        TimeZone: 'Pacific Standard Time',
+      },
+    };
 
+    // eslint-disable-next-line camelcase
     const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
       headers: ToDo.getAuthHeaders(this.accessToken),
-      payload: data,
+      payload: JSON.stringify(data),
       method: 'post',
-      contentType: 'application/json'
-    }
+      contentType: 'application/json',
+    };
 
-    const response = this.fetchApp.fetch(`${ToDo.BASE_URL}/taskfolders('${folderID}')/tasks`, options)
-    return JSON.parse(response.getContentText())
+    const response = this.fetchApp.fetch(`${ToDo.BASE_URL}/taskfolders('${folderID}')/tasks`, options);
+    return JSON.parse(response.getContentText());
   }
-
 }
 
 export default ToDo;
